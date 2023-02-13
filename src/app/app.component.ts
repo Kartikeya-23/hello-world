41644e95e783c6658f65fecdd242b4fab085c6ef
Mukul Verma <mukul.verma@Geminisolutions.com>
2023-02-13 17:15:22 +0530

    Angular Course by Mosh: section-4 completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hello-world';
+  email :string = 'ka@gs.com';
+  course = {
+    title: "The Complete Angular Course",
+    rating: 4.9756,
+    students: "30000",
+    price: 190.95,
+    releaseDate: new Date(2016, 3, 1)
+  }
+  text: string = "The way I worked in Gemini Solution working as an Angular Developer without having the knowledge of Angular is just Awesome and disgusting at the same time."
+  onSave($event: any){
+    console.log("Button was clicked")
+    console.log($event);
+  }
+  // onKeyUp($event : any){
+  //   if($event.keyCode === 13) {
+  //     console.log("Enter was pressed");
+  //     console.log("value of input is ", $event.target.value);
+  //   }
+  //   console.log($event.target.value);
+  // }
+  onKeyUp(){
+    console.log(this.email);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
-import { NgModule } from '@angular/core';
+import { NgModule, } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SummaryPipe } from './summary.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SummaryPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
